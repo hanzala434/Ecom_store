9247eb080ef6f419c0785ea8923121e72737f836
hanzala434 <hanzalay959@gmail.com>
2024-12-09 01:05:52 +0500

    address_text

--- frontend/src/components/AddressFrom.jsx
+++ frontend/src/components/AddressFrom.jsx
@@ -55,7 +55,7 @@ const AddressFrom = () => {
   <h1 className='flex justify-center p-2 font-bold'>Enter Your Address Details</h1>
 <form onSubmit={handleSubmit} className="max-w-md mx-auto">
   <div className="relative z-0 w-full mb-5 group">
-      <input type="text" name="address" value={address} onChange={onChange} id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+      <input type="text" name="address" value={address} onChange={onChange} id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-slate-600 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
       <label for="floating_email" className="peer-focus:font-medium absolute text-sm
        text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 
        top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 
